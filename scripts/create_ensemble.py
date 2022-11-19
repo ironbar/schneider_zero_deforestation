66de7e31e4abe67e0b18db331865ad8e440044fb
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2022-11-19 22:02:59 +0100

    typed hints

--- scripts/create_ensemble.py
+++ scripts/create_ensemble.py
@@ -2,6 +2,7 @@ import os
 import sys
 import argparse
 import glob
+from typing import List
 import numpy as np
 import pandas as pd
 
@@ -29,7 +30,7 @@ def get_val_labels(data_path: str) -> pd.DataFrame:
     return val_labels
 
 
-def load_preds(predictions_dir):
+def load_preds(predictions_dir:str ):
     val_pred_filepaths = sorted(glob.glob(os.path.join(predictions_dir, '*', 'val_preds.csv')))
     val_preds = np.array([np.loadtxt(filepath, delimiter=',') for filepath in val_pred_filepaths])
     test_preds = np.array([np.loadtxt(filepath.replace('val_preds', 'test_preds'), delimiter=',') 
@@ -38,18 +39,19 @@ def load_preds(predictions_dir):
     return val_preds, test_preds, names
 
 
-def get_single_model_scores(val_preds, val_labels, names):
+def get_single_model_scores(val_preds: np.ndarray, val_labels: np.ndarray, names: List[str]) -> np.ndarray:
     scores = [_get_f1_score(val_labels, val_pred) for val_pred in val_preds]
     print('Single model scores:')
     print(pd.DataFrame({'score': scores, 'name': names}).sort_values('score', ascending=False).reset_index(drop=True))
-    return scores
+    return np.array(scores)
 
 
-def _get_f1_score(val_labels, val_preds):
+def _get_f1_score(val_labels: np.ndarray, val_preds: np.ndarray) -> float:
     return round(f1_score(val_labels, np.argmax(val_preds, axis=1), average='macro'), 4)
 
 
-def create_ensemble(val_preds, val_labels, val_scores, test_preds, best_n_models):
+def create_ensemble(val_preds: np.ndarray, val_labels: np.ndarray, val_scores: np.ndarray,
+                    test_preds: np.ndarray, best_n_models: int) -> np.ndarray:
     print('\nScore of the ensemble depending on the number of models used:')
     for idx in range(1, 30):
         weights = np.ones(len(val_scores))
@@ -67,7 +69,7 @@ def create_ensemble(val_preds, val_labels, val_scores, test_preds, best_n_models
     return test_ensemble
 
 
-def ensemble_predictions(preds, weights):
+def ensemble_predictions(preds: np.ndarray, weights: np.ndarray) -> np.ndarray:
     weights = np.expand_dims(np.expand_dims(weights, axis=1), axis=2)
     return np.argmax(np.mean(weights*preds, axis=0), axis=1)
 
