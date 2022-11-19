b4d98cbc1ac49f04aa3ff45c9e1bdb40d0099f54
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2022-11-19 11:29:20 +0100

    created keras finetuning script

---
+++ scripts/finetune_keras_imagenet.py
@@ -0,0 +1,134 @@
+"""
+Finetune ResNet50 for schneider deforestation challenge
+
+https://keras.io/api/applications/
+https://keras.io/api/applications/resnet/#resnet50-function
+https://keras.io/examples/vision/image_classification_efficientnet_fine_tuning/
+"""
+import os
+import sys
+import argparse
+import pandas as pd
+from tqdm import tqdm
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+
+from tensorflow import keras
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import preprocess_input, ResNet50
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow_addons.metrics import F1Score
+
+
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+    args = parse_args(args)
+    train_data, val_data, x_test, ohe = load_data(args.data_path)
+    model, base_model = create_model()
+    finetune_model(model, base_model, train_data, val_data, finetune_backbone=False)
+    finetune_model(model, base_model, train_data, val_data, finetune_backbone=True)
+
+
+def load_data(data_path):
+    train_df = _load_dataframe(data_path, 'train.csv')
+    test_df = _load_dataframe(data_path, 'test.csv')
+    train_imgs = _load_imgs(train_df.example_path)
+    test_imgs = _load_imgs(test_df.example_path)
+    inputs = _preprocess_inputs(train_imgs)
+    x_test = _preprocess_inputs(test_imgs)
+
+    ohe = OneHotEncoder(sparse=False)
+    ohe_labels = ohe.fit_transform(np.expand_dims(np.array(train_df.label), 1))
+
+    x_train, x_val, y_train, y_val = train_test_split(
+        inputs, ohe_labels, test_size=0.2, random_state=7, stratify=train_df.label)
+
+    return (x_train, y_train), (x_val, y_val), x_test, ohe
+
+
+def _load_dataframe(data_path, filename):
+    df = pd.read_csv(os.path.join(data_path, filename))
+    df.example_path = df.example_path.apply(lambda x: os.path.join(data_path, x))
+    return df
+
+
+def _load_imgs(image_filepaths, img_side=224):
+    imgs = [image.load_img(image_filepath, target_size=(img_side, img_side))
+            for image_filepath in tqdm(image_filepaths)]
+    return imgs
+
+def _preprocess_inputs(imgs):
+    inputs = np.array([preprocess_input(image.img_to_array(img)) for img in tqdm(imgs)],
+                       dtype=np.float32)
+    return inputs
+
+
+def create_model(n_categories=3, img_side=224):
+    img_augmentation = Sequential(
+        [
+            #keras.layers.RandomRotation(factor=0.15),
+            #keras.layers.RandomTranslation(height_factor=0.1, width_factor=0.1),
+            keras.layers.RandomFlip(),
+            #keras.layers.RandomContrast(factor=0.1),
+        ],
+        name="img_augmentation",
+    )
+    inputs = keras.layers.Input(shape=(img_side, img_side, 3))
+    x = img_augmentation(inputs)
+    base_model = ResNet50(weights='imagenet', include_top=False)
+    outputs = base_model(x, training=False)
+    outputs = GlobalAveragePooling2D()(outputs)
+    #outputs = Dense(1024, activation='relu')(outputs)
+    outputs = Dense(n_categories, activation='softmax')(outputs)
+    model = Model(inputs=inputs, outputs=outputs)
+    return model, base_model
+
+
+def finetune_model(model, base_model, train_data, val_data, finetune_backbone, n_categories=3):
+    base_model.trainable = finetune_backbone
+    if finetune_backbone:
+        print('\tFine-tuning the whole model')
+        optimizer = keras.optimizers.SGD(learning_rate=1e-4, momentum=0.9)
+    else:
+        print('\tFine-tuning only the top layers')
+        optimizer = 'Adam'
+    model.compile(optimizer=optimizer, loss='categorical_crossentropy',
+                  metrics=[F1Score(n_categories), 'categorical_accuracy'])
+    callbacks = [
+        EarlyStopping(monitor='val_f1_score', mode='max', patience=20, restore_best_weights=True),
+        keras.callbacks.ReduceLROnPlateau(monitor='val_loss', patience=3, factor=0.8),
+    ]
+    model.fit(*train_data, validation_data=val_data, epochs=100, batch_size=128, callbacks=callbacks)
+    print('\nBest validation score:')
+    model.evaluate(*val_data)
+
+
+class EarlyStopping(keras.callbacks.EarlyStopping):
+    def get_monitor_value(self, logs):
+        logs = logs or {}
+        monitor_value = logs.get(self.monitor)
+        if monitor_value is None:
+            raise ValueError(f'Early stopping requires {self.monitor} available!')
+        return np.mean(monitor_value)
+
+
+def parse_args(args):
+    epilog = """
+    """
+    description = """
+    Fine-tune ResNet50 for schneider deforestation challenge
+    """
+    parser = argparse.ArgumentParser(
+        description=description,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        epilog=epilog)
+    parser.add_argument('--data_path', help='Path to folder with the data',
+                        default='/mnt/hdd0/Kaggle/schneider_deforestation/data')
+    return parser.parse_args(args)
+
+
+if __name__ == '__main__':
+    main()
